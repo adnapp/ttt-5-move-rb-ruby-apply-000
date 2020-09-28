bf596c1f027f4c6733270734bbc652c215492ea4
Adam Napoletano <adam.napps@gmail.com>
2020-09-28 01:13:55 +0000

    Automatically backed up by Learn

--- lib/move.rb
+++ lib/move.rb
@@ -10,8 +10,7 @@ end
 def input_to_index(input)
   input = input.to_i
   index = input - 1
-  return index
 end
-def move(board, index, value)
+def move(board, index, value = "X")
   board[index] = value
 end
